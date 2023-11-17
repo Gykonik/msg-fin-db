afff22378f9d8db438a84af97508536ba388e9a0
chakirLbr <chakirlbr@gmail.com>
2023-11-17 18:17:05 +0100

    backend repository

---
+++ backend/demo/src/test/java/msgfindb/backend/MsgFinDbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package msgfindb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgFinDbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
